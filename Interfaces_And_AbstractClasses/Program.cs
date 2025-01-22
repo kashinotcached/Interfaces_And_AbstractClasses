17e8c709e4c038d33f0cf7ae74359b70d4981f30
kashinotcached <kashinathnagvekar1@gmail.com>
2025-01-22 19:36:52 +0530

    Add project files.

---
+++ Interfaces_And_AbstractClasses/Program.cs
@@ -0,0 +1,13 @@
+﻿//Interfaces And Abstract Classes
+
+/*
+What is an Interface?
+    - Contains a group of related functions that any class that implements the interface must implement.
+    - Important because C# doesn't support multiple inheritance of classes.
+    - Interfaces are a workaround for multiple inheritance.
+
+* Think of an interface like a contract.
+* Class agrees to implement all of the functions inside the interface.
+* A class or struct can implement one or more interfaces.
+
+ */
