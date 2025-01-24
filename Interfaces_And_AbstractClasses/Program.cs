37db51d561c86d6e37aec5857847a8880c5e6735
kashinotcached <kashinathnagvekar1@gmail.com>
2025-01-24 10:17:17 +0530

    IEnumerator interface

--- Interfaces_And_AbstractClasses/Program.cs
+++ Interfaces_And_AbstractClasses/Program.cs
@@ -258,58 +258,89 @@ What is an Interface?
 
 /* ----------------------------------------------------------------------------------------------------------- */
 
-//IEnumerable Interface
+////IEnumerable Interface
 
-class Wallet : IEnumerable
+//class Wallet : IEnumerable
+//{
+//    Money[] bills = null;
+//    int openIndex = 0;
+
+//    public Wallet()
+//    {
+//        bills = new Money[100];
+//    }
+
+//    public void Add(Money bill)
+//    {
+//        bills[openIndex] = bill;
+//        openIndex++;
+//    }
+
+//    public IEnumerator GetEnumerator()
+//    {
+//        foreach(Money bill in bills)
+//        {
+//            if(bill == null)
+//            {
+//                break;
+//            }
+
+//            yield return bill;
+//        }
+//    }
+//}
+
+//class Money
+//{
+//    public int amount;
+//}
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Wallet wallet = new Wallet();
+//        wallet.Add(new Money() { amount = 1 });
+//        wallet.Add(new Money() { amount = 5 });
+//        wallet.Add(new Money() { amount = 10 });
+//        wallet.Add(new Money() { amount = 20 });
+//        wallet.Add(new Money() { amount = 50 });
+//        wallet.Add(new Money() { amount = 100 });
+
+//        foreach(Money money in wallet)
+//        {
+//            System.Diagnostics.Debug.WriteLine( $"Bills : {money.amount}");
+//        }
+//    }
+//}
+
+
+/* ----------------------------------------------------------------------------------------------------------- */
+
+//IEnumerator Interface
+
+class Program : IEnumerator
 {
-    Money[] bills = null;
-    int openIndex = 0;
+    List<object> items = new List<object>();
+    int current;
 
-    public Wallet()
-    {
-        bills = new Money[100];
-    }
+    public object Current => items[current];
 
-    public void Add(Money bill)
-    {
-        bills[openIndex] = bill;
-        openIndex++;
-    }
-
-    public IEnumerator GetEnumerator()
-    {
-        foreach(Money bill in bills)
-        {
-            if(bill == null)
-            {
-                break;
-            }
-
-            yield return bill;
-        }
-    }
-}
-
-class Money
-{
-    public int amount;
-}
-
-class Program
-{
     static void Main(string[] args)
     {
-        Wallet wallet = new Wallet();
-        wallet.Add(new Money() { amount = 1 });
-        wallet.Add(new Money() { amount = 5 });
-        wallet.Add(new Money() { amount = 10 });
-        wallet.Add(new Money() { amount = 20 });
-        wallet.Add(new Money() { amount = 50 });
-        wallet.Add(new Money() { amount = 100 });
 
-        foreach(Money money in wallet)
-        {
-            System.Diagnostics.Debug.WriteLine( $"Bills : {money.amount}");
-        }
+    }
+
+    public bool MoveNext()
+    {
+        if(items.Count == 0 || items.Count <= current)
+            return false;
+
+        return true;
+    }
+
+    public void Reset()
+    {
+        current = 0;
     }
 }
